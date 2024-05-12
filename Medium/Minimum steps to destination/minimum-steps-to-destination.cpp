2e1dd0a63329a0cbbf27fcdecc2279895c6249ab
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-05-12 23:27:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum steps to destination/minimum-steps-to-destination.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minSteps(int d) {
+        int sum=0,step=0;
+        while(sum<d || abs(sum-d)%2!=0){
+            step++;
+            sum+=step;
+        }
+        return step;
+        
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int d;
+        cin >> d;
+
+        Solution ob;
+        cout << ob.minSteps(d) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
