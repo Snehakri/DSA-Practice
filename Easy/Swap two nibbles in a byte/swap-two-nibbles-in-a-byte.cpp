b18926a447d3068824ac00813d5e26cb3848a14a
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-05-31 10:53:19 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Swap two nibbles in a byte/swap-two-nibbles-in-a-byte.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        int a=n & (0b11110000);
+        int b=n & (0b00001111);
+        a=a>>4;
+        b=b<<4;
+        
+        return a|b;
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.swapNibbles(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
