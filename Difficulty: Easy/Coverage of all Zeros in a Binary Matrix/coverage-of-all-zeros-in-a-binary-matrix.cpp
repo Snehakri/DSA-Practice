b87b61bd474bed93a6097de270e5265051e7e6a8
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-06-26 18:09:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Coverage of all Zeros in a Binary Matrix/coverage-of-all-zeros-in-a-binary-matrix.cpp
+++ Difficulty: Easy/Coverage of all Zeros in a Binary Matrix/coverage-of-all-zeros-in-a-binary-matrix.cpp
@@ -8,68 +8,51 @@ using namespace std;
 // } Driver Code Ends
 
 
-// class Solution {
-//   public:
-//     int findCoverage(vector<vector<int>>& matrix) {
-//         int n=matrix.size();
-//         int m=matrix[0].size();
-//         // vector<vector<int>> v(n,vector<int> (m,0));
-//         // for(int i=0;i<n;i++){
-//         //     for(int j=0;j<m;j++){
-//         //         if(i==0){
-//         //             v[i][j]=matrix[i][j];
-//         //         }
-//         //         else if(j==0){
-//         //             v[i][j]=matrix[i][j]+matrix[i-1][j];
-//         //         }
-//         //         else {
-//         //             v[i][j]=matrix[i][j]+matrix[i-1][j] +matrix[i][j-1];
-//         //         }
-//         //     }
-//         // }
-//         int sum=0;
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<m;j++){
-//                 if(matrix[i][j]==1) {
-//                     continue;
-//                 }
-//                 if(i-1>=0 && matrix[i-1][j]==1 ) sum++;
-//                 if(i+1<n && matrix[i+1][j]==1) sum++;
-//                 if(j-1>=0 && matrix[i][j-1]==1 ) sum++;
-//                 if(j+1<n && matrix[i][j+1]==1 ) sum++;
-//             }
-//         }
-//         return sum;
-//         // Code here
-//     }
-// };
 class Solution {
-public:
-    int findCoverage(vector<vector<int>>&matrix){
-        // CodeGenius
-        // i-1>=0 , i+1<n
-        //j-1>=0 , j+1<n
-        int ans =0;
+  public:
+    int findCoverage(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
+        int sum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==1){
+                if(matrix[i][j]==1) {
                     continue;
                 }
-                if(i-1>=0 and matrix[i-1][j]==1)
-                ans++;
-                if(i+1<n and matrix[i+1][j]==1)
-                ans++;
-                if(j-1>=0 and matrix[i][j-1]==1)
-                ans++;
-                if(j+1<m and matrix[i][j+1]==1)
-                ans++;
+                if(i-1>=0 && matrix[i-1][j]==1 ) sum++;
+                if(i+1<n && matrix[i+1][j]==1) sum++;
+                if(j-1>=0 && matrix[i][j-1]==1 ) sum++;
+                if(j+1<m && matrix[i][j+1]==1 ) sum++;
             }
         }
-        return ans;
+        return sum;
+        // Code here
     }
 };
+// class Solution {
+// public:
+//     int findCoverage(vector<vector<int>>&matrix){
+//         int ans =0;
+//         int n=matrix.size();
+//         int m=matrix[0].size();
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(matrix[i][j]==1){
+//                     continue;
+//                 }
+//                 if(i-1>=0 and matrix[i-1][j]==1)
+//                 ans++;
+//                 if(i+1<n and matrix[i+1][j]==1)
+//                 ans++;
+//                 if(j-1>=0 and matrix[i][j-1]==1)
+//                 ans++;
+//                 if(j+1<m and matrix[i][j+1]==1)
+//                 ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
 
 //{ Driver Code Starts.
