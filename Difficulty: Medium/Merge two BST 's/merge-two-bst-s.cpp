4615b15edff9f7de91c67bb5cdbf65009d7f549a
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-09-30 19:05:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -94,42 +94,21 @@ struct Node {
 */
 class Solution {
   public:
-    // Function to return a list of integers denoting the node
-    // values of both the BST in a sorted order.
-    void inorder(Node *root,vector<int> &v){
-        if(!root) return ;
-        inorder(root->left,v);
-        v.push_back(root->data);
-        inorder(root->right,v);
-    }
+  void r(Node *root, vector<int>&v){
+      if(root==nullptr) return;
+      v.push_back(root->data);
+      r(root->left,v);
+      r(root->right,v);
+
+  }
     vector<int> merge(Node *root1, Node *root2) {
-        vector<int> v1;
-        vector<int> v2;
-        vector<int> ans;
-        inorder(root1,v1);
-        inorder(root2,v2);
-        int i=0,j=0;
-        while(i<v1.size() && j<v2.size()){
-            if(v1[i]<v2[j]){
-                ans.push_back(v1[i]);
-                i++;
-            }
-            else{
-                ans.push_back(v2[j]);
-                j++;
-            }
-        }
-        while(i<v1.size()){
-             ans.push_back(v1[i]);
-            i++;
-        }
-        while(j<v2.size()){
-             ans.push_back(v2[j]);
-                j++;
-        }
+        vector<int>ans;
+        r(root1,ans);
+        r(root2,ans);
+        sort(ans.begin(),ans.end());
         return ans;
-        // Your code here
     }
+
 };
 
 //{ Driver Code Starts.
