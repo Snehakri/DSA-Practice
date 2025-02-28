2e7e87512d9cec79e715a7676da923ff83abfe2b
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2025-02-28 18:08:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Evaluation of Postfix Expression/evaluation-of-postfix-expression.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int evaluate(vector<string>& arr) {
+        // code here
+        stack<int>st;
+        for(int i=0;i<arr.size();i++){
+            if((arr[i]=="+")||(arr[i]=="-") || (arr[i]=="*") ||
+            (arr[i]=="/")  ){
+                int first=st.top();
+                st.pop();
+                
+                int second=st.top();
+                st.pop();
+                if(arr[i]=="+"){
+                
+                    st.push(first+second);
+                }
+                else if(arr[i]=="-"){
+                
+                    st.push(second-first);
+                }
+                else if(arr[i]=="*"){
+                    
+                    st.push(first*second);
+                }
+                else if(arr[i]=="/"){
+                    
+                    st.push(second/first);
+                }
+                
+            }
+            else{
+                st.push(stoi(arr[i]));
+            }
+        }
+        return st.top();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string str;
+    getline(cin, str);
+    int t = stoi(str);
+    while (t--) {
+        getline(cin, str);
+        stringstream ss(str);
+        vector<string> arr;
+        while (ss >> str) {
+            arr.push_back(str);
+        }
+        Solution ob;
+        cout << ob.evaluate(arr) << endl;
+        cout << "~" << endl;
+    }
+}
+// } Driver Code Ends
