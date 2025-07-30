2d65e6456eb3a018a314e07f0d32947b0357aa03
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2025-07-30 22:08:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
@@ -1,62 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
   public:
-    
-int countSubarrays(vector<int> &arr, int k) {
-        // 10 12 10 -10 0
-        unordered_map<int, int> mp;
-        mp[0] = 1;
-        int sum = 0;
-        int ans = 0;
-        for (auto it: arr){ 
-            sum += it;
-            int rem = sum - k; // search this reminder in the hashmap
-            if (mp.find(rem) != mp.end()){
-                ans += mp[rem];
-            }
-            mp[sum]++;
+    int cntSubarrays(vector<int> &arr, int k) {
+        unordered_map<int, int> prefixSums;
+        int res = 0;
+        int currSum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            currSum += arr[i];
+            if (currSum == k)
+                res++;
+            if (prefixSums.find(currSum - k) != prefixSums.end())
+                res += prefixSums[currSum - k];
+            prefixSums[currSum]++;
         }
-        
-        return ans;
+        return res;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        cout << obj.countSubarrays(arr, k);
-        cout << endl;
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
